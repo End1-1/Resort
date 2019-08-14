8de38229df7c9f9321d81fe812f105cd3f7a7372
End1-1 <end1_1@yahoo.com>
2019-08-14 14:42:36 +0400

    Revert "Yesim"
    This reverts commit 16c16e743ea28d31a4fa680851bba27156f281cc.

--- Resort/rc.h
+++ Resort/rc.h
@@ -1,8 +1,8 @@
 #ifndef RC_H
 #define RC_H
 
-#define _PRODUCT_VERSION 1,8,19,797
-#define _FILE_VERSION_STR "1.8.19.797\0"
+#define _PRODUCT_VERSION 1,8,19,796
+#define _FILE_VERSION_STR "1.8.19.796\0"
 #define _PRODUCT_VERSION_STR "1.8\0"
 
 #endif // RC_H
